fc95dc62bbc205f90b048fdb87f3268d3dd2dbce
samuel farias <samuelcontatoe1@gmail.com>
2024-04-16 20:13:21 -0300

    licao total concluida

--- js/script.js
+++ js/script.js
@@ -18,5 +18,39 @@ let admin = name;
 alert(admin);
 */
 
-let planet = "terra";
-let user = "Sam";
+//let planet = "terra";
+//let user = "Sam";
+
+/*let a = 1, b = 1;
+
+let c = ++a;
+let d = b++;
+
+console.log(a,b,c,d)
+
+*/
+/*
+let a = 2;
+
+let x = 1 + (a *= 2);
+console.log(x);
+*/
+
+console.log(23+97);
+console.log(1+3+5+7+9+11);
+
+console.log((4+6+9)/77)
+let a = 10;
+console.log(a);
+
+console.log(9*a);
+
+let b = 7*a;
+console.log(b);
+
+
+const MAX = 57;
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
+
