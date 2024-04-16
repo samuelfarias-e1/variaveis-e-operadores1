f3ced6862efec7dc35f7e980839e7291565173b1
samuel farias <samuelcontatoe1@gmail.com>
2024-04-16 19:58:41 -0300

    licao concluida

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+let user = 'sam';
+let age = 25
+let hello = 'Hello World';
+let message = hello;
+
+const myBirthday = '13.10.2003';
+
+alert(myBirthday);
+alert(message);
+
+*/
+
+
+/*let name = "John";
+let admin = name;
+
+alert(admin);
+*/
+
+let planet = "terra";
+let user = "Sam";
